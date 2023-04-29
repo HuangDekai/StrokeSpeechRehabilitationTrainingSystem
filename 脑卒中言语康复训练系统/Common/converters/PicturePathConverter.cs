b738497f29263045b0c5a1628dd6509d76e1cf63
黄德铠 <470574122@qq.com>
2023-04-29 21:07:35 +0800

    将题目读图变为相对路径

---
+++ 脑卒中言语康复训练系统/Common/converters/PicturePathConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace 脑卒中言语康复训练系统.Common.converters
+{
+    internal class PicturePathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string fileName = value as string;
+            if (fileName != null)
+            {
+                var baseDirectory = System.AppDomain.CurrentDomain.BaseDirectory.Replace("\\", "/");
+                var imagePath = Path.Combine("Image", fileName);
+                var imagePathRelativeToBaseDirectory = Path.Combine(baseDirectory, imagePath);
+                fileName = imagePathRelativeToBaseDirectory.ToString();
+            }
+            return fileName;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
